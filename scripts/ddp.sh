065ac104e135598681627122f8d8e79428b5d232
Sidd Karamcheti <skaramcheti@cs.stanford.edu>
2021-03-18 02:38:57 -0700

    Fairscale Z3 - Single Node :: Verified

--- scripts/ddp.sh
+++ scripts/ddp.sh
@@ -6,7 +6,7 @@ nnodes=${1:-1}
 node_rank=${2:-0}
 
 # Default Configuration of GPUs on the Sphinx Machines
-GPUS_PER_NODE=8
+GPUS_PER=8
 
 # Assumes `sphinx1` is the main node - node rank must be 0 on sphinx1!
 MASTER_ADDR=sphinx1.stanford.edu
@@ -14,10 +14,10 @@ MASTER_PORT=7000
 WORLD_SIZE=$((${nnodes}*${node_rank}))
 
 # `torch.distributed.launch` Parameters
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes ${nnodes} --node_rank ${node_rank} --master_addr $MASTER_ADDR"
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER --nnodes ${nnodes} --node_rank ${node_rank} --master_addr $MASTER_ADDR"
 
 # Default `train.py` config arguments
-CONFIG_ARGS="--config conf/gpt2-sphinx-debug-config.yaml --nproc_per_node $GPUS_PER_NODE --nnodes ${nnodes}"
+CONFIG_ARGS="--config conf/gpt2-sphinx-debug-config.yaml --nproc_per_node $GPUS_PER --nnodes ${nnodes}"
 
 # export NCCL_DEBUG=INFO; \
 python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py $CONFIG_ARGS
--- scripts/fairscale.sh
+++ scripts/fairscale.sh
@@ -6,7 +6,7 @@ nnodes=${1:-1}
 node_rank=${2:-0}
 
 # Default Configuration of GPUs on the Sphinx Machines
-GPUS_PER_NODE=8
+GPUS_PER=8
 
 # Assumes `sphinx1` is the main node - node rank must be 0 on sphinx1!
 MASTER_ADDR=sphinx1.stanford.edu
@@ -14,21 +14,26 @@ MASTER_PORT=7000
 WORLD_SIZE=$((${nnodes}*${node_rank}))
 
 # `torch.distributed.launch` Parameters
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes ${nnodes} --node_rank ${node_rank} --master_addr $MASTER_ADDR"
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER --nnodes ${nnodes} --node_rank ${node_rank} --master_addr $MASTER_ADDR"
 
 # Default `train.py` config arguments
-CONFIG_ARGS="--config conf/gpt2-sphinx-debug-config.yaml --nproc_per_node $GPUS_PER_NODE --nnodes ${nnodes}"
+CONFIG_ARGS="--config conf/gpt2-sphinx-debug-config.yaml --nproc_per_node $GPUS_PER --nnodes ${nnodes}"
 
 # FairScale Parameters
 FAIRSCALE_Z1="--training_arguments.sharded_ddp simple"
 FAIRSCALE_Z2="--training_arguments.sharded_ddp zero_dp_2+auto_wrap"
+FAIRSCALE_Z3="--training_arguments.sharded_ddp zero_dp_3+auto_wrap"
+FAIRSCALE_Z2_OFF="--training_arguments.sharded_ddp zero_dp_2+auto_wrap+offload"
 
 # export NCCL_DEBUG=INFO; \
 # =>> ZeRO-1 (Simple)
 # python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py $CONFIG_ARGS $FAIRSCALE_Z1
 
-# ==> ZeRO-2
-python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py $CONFIG_ARGS $FAIRSCALE_Z2
+# =>> ZeRO-2
+# python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py $CONFIG_ARGS $FAIRSCALE_Z2
+
+# =>> ZeRO-3
+python -m torch.distributed.launch $DISTRIBUTED_ARGS train.py $CONFIG_ARGS $FAIRSCALE_Z3
 
 # Kill Running Processes (Because `torch.distributed.launch` doesn't like to clean up after itself...)
 pkill -f "train.py"
