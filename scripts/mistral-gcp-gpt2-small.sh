412d71fb64b1672a180625f2a656b663cbe5739b
lorr1 <57237365+lorr1@users.noreply.github.com>
2021-04-14 21:15:06 -0600

    Fixed launching bash script with resume/no resume, run id, and mdel name

--- scripts/mistral-gcp-gpt2-small.sh
+++ scripts/mistral-gcp-gpt2-small.sh
@@ -2,9 +2,39 @@
 #   Mistral GPT-2 Small Full Run with the DeepSpeed ZeRO-2 Optimizer, Per-Device Batch Size of 16 on Google Cloud with
 #   MegaGPU Instances.
 
+# Parse Named Command Arguments::
+#   EX: bash mistral-gcp-gpt2-small.sh MODEL="cyclone" RESUME="true"
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+
+    case "$KEY" in
+            MODEL)              MODEL=${VALUE} ;;
+            RESUME)             RESUME=${VALUE} ;;
+            *)
+    esac
+
+done
+
+# Set to Default Values if Param is not Set
+if [ -z "$MODEL" ]; then MODEL='downpour'; fi
+if [ -z "$RESUME" ]; then RESUME='false'; fi
+
+echo "MODEL = $MODEL"
+echo "RESUME = $RESUME"
+
 # Constants
-CONFIG="--config conf/gpt2-mistral-config.yaml"
-GCP_CONFIG="--config conf/gpt2-mistral-gcp-config.yaml"
+CONFIG="--config conf/gpt2-mistral-config.yaml";
+GCP_CONFIG="--config conf/gpt2-mistral-gcp-config.yaml";
+if [ "$RESUME" == "false" ];
+then
+  RES="--resume false";
+else
+  RES="--resume true";
+fi
+
 INFRA="--nnodes 1 --nproc_per_node 16"
 
 # Batch Size
@@ -13,27 +43,41 @@ D_BSZ_16="--training_arguments.fp16 true --training_arguments.per_device_train_b
 # DeepSpeed Training Configuration
 DS_Z2="--training_arguments.deepspeed conf/deepspeed/z2-conf.json"
 
-# Random Seeds -- Cyclone :: 81, Downpour :: 343, Eddy :: 777
-CYCLONE="--seed 81"
-DOWNPOUR="--seed 343"
-EDDY="--seed 777"
+# Random Seeds -- Aurora :: 21, Blizzard :: 49, Cyclone :: 81, Downpour :: 343, Eddy :: 777
+case $MODEL in
+   aurora)
+     SEED="--seed 21"
+     RUN_ID="--run_id aurora-gpt2-small-x21"
+     ;;
+   blizzard)
+     SEED="--seed 49"
+     RUN_ID="--run_id blizzard-gpt2-small-x49"
+     ;;
+   cyclone)
+     SEED="--seed 81"
+     RUN_ID="--run_id cyclone-gpt2-small-x81"
+     ;;
+   downpour)
+     SEED="--seed 343"
+     RUN_ID="--run_id downpour-gpt2-small-x343"
+     ;;
+   eddy)
+     SEED="--seed 777"
+     RUN_ID="--run_id eddy-gpt2-small-x777"
+     ;;
+   ?)
+     usage
+     exit
+     ;;
+ esac
 
 # Set DeepSpeed Launcher Parameters
 DISTRIBUTED_ARGS="--num_gpus 16 --num_nodes 1"
 
 # ---
 
-# Cyclone: Single-Node DS-Z2, Linear LR Schedule, Device BSZ = 16 --> Cleanup --> Seed =>> Seed 81
-deepspeed $DISTRIBUTED_ARGS train.py $GCP_CONFIG $INFRA $D_BSZ_16 $CYCLONE $DS_Z2 --run_id cyclone-gpt2-small-x81
-pkill -f "train.py"
-sleep 3
-
-# Downpour: Single-Node DS-Z2, Linear LR Schedule, Device BSZ = 16 --> Cleanup --> Seed =>> Seed 343
-deepspeed $DISTRIBUTED_ARGS train.py $GCP_CONFIG $INFRA $D_BSZ_16 $DOWNPOUR $DS_Z2 --run_id downpour-gpt2-small-343
-pkill -f "train.py"
-sleep 3
-
-# Eddy: Single-Node DS-Z2, Linear LR Schedule, Device BSZ = 16 --> Cleanup --> Seed =>> Seed 777
-deepspeed $DISTRIBUTED_ARGS train.py $GCP_CONFIG $INFRA $D_BSZ_16 $EDDY $DS_Z2 --run_id eddy-gpt2-small-777
+# Single-Node DS-Z2, Linear LR Schedule, Device BSZ = 16 --> Cleanup --> Seed
+echo deepspeed $DISTRIBUTED_ARGS train.py $GCP_CONFIG $INFRA $D_BSZ_16 $SEED $RES $DS_Z2 $RUN_ID
+deepspeed $DISTRIBUTED_ARGS train.py $GCP_CONFIG $INFRA $D_BSZ_16 $SEED $RES $DS_Z2 $RUN_ID
 pkill -f "train.py"
 sleep 3
