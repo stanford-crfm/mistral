e5bbb323cd95abea0bbdbff4ef76af435916c3f4
Sidd Karamcheti <skaramcheti@cs.stanford.edu>
2021-04-19 13:12:59 -0700

    Add GPT-2 Medium config

--- scripts/mistral-gpt2-medium.sh
+++ scripts/mistral-gpt2-medium.sh
@@ -1,5 +1,5 @@
 # mistral-gpt2-medium.sh
-#   Mistral GPT-2 Medium Dry-Runs with the DeepSpeed ZeRO-2 Optimizer, Per-Device Batch Size of 16/8/4.
+#   Mistral GPT-2 Medium Full Run with the DeepSpeed ZeRO-2 Optimizer, Per-Device Batch Size of 4
 
 # Constants
 CONFIG="--config conf/gpt2-mistral-medium-config.yaml"
@@ -9,7 +9,12 @@ INFRA="--nnodes 2 --nproc_per_node 8"
 D_BSZ_4="--training_arguments.fp16 true --training_arguments.per_device_train_batch_size 4"
 
 # DeepSpeed Training Configuration
-DS_Z2="--training_arguments.deepspeed conf/deepspeed/z2-conf.json"
+DS_Z2="--training_arguments.deepspeed conf/deepspeed/z2-medium-conf.json"
+
+# Random Seeds -- Arwen :: 21, Beren :: 49, Celebrimbor :: 81
+ARWEN="--seed 21"
+BEREN="--seed 49"
+CELEBRIMBOR="--seed 81"
 
 # Set DeepSpeed Launcher Parameters
 MASTER_ADDR=sphinx1.stanford.edu
@@ -19,6 +24,6 @@ DISTRIBUTED_ARGS="--num_gpus 8 --num_nodes 2 --master_addr $MASTER_ADDR"
 # ---
 
 # Multi-Node DS-Z2, Linear LR Schedule, Device BSZ = 4 --> Cleanup --> Sleep
-deepspeed $DISTRIBUTED_ARGS train.py $CONFIG $INFRA $D_BSZ_4 $DS_Z2 --run_id gpt2-medium-dry-run-dbsz=4-no-gc
+deepspeed $DISTRIBUTED_ARGS train.py $CONFIG $INFRA $D_BSZ_4 $ARWEN $DS_Z2 --run_id arwen-gpt2-medium-x21
 pkill -f "train.py"
 sleep 3
