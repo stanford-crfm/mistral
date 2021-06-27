783e5663ef6b5b36c484e28ad85f84eea18d34ea
J38 <jebolton@stanford.edu>
2021-06-27 01:04:04 -0700

    deal with cache issue via template

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_css_files = ["readthedocs-custom-v1.css"] # Override some CSS settings
+html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
 
 # Pydata theme
 #html_theme = "pydata_sphinx_theme"
