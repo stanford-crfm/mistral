d21ce84f4d84a8b09b0fd51c5a0aed86d7d35a19
J38 <jebolton@stanford.edu>
2021-07-29 04:21:42 -0700

    add github mode

--- docs/scripts/build_download_tables.py
+++ docs/scripts/build_download_tables.py
@@ -4,6 +4,8 @@ create_download_tables.py
 Build and verify download link table.
 """
 
+import argparse
+
 import requests
 
 
@@ -12,7 +14,33 @@ def verify_download_link(link):
     return results.ok
 
 
-def produce_download_tables():
+def github_table_header(size):
+    return (
+        f"\nGPT-2 {size.capitalize()}\n\n| Run | Type | Checkpoint | Size | Link |\n| --- | --- | --- | --- | --- |\n"
+    )
+
+
+def github_table_row(run, size, checkpoint, download_size, download_link):
+    return f"| {run} | GPT-2 {size.capitalize()} | {checkpoint} | {download_size} | [download]({download_link}) |\n"
+
+
+def rst_table_header(size):
+    return (
+        f".. csv-table:: GPT-2 {size.capitalize()} Models\n"
+        '   :header: "Run", "Type", "Checkpoint", "Size", "Link"\n'
+        "   :widths: 7,7,7,5,7\n\n"
+    )
+
+
+def rst_table_row(run, size, checkpoint, download_size, download_link):
+    return f'   "{run}", "GPT-2 {size.capitalize()}", "{checkpoint}", {download_size}, `download <{download_link}>`_\n'
+
+
+table_header_creators = {"github": github_table_header, "rst": rst_table_header}
+row_creators = {"github": github_table_row, "rst": rst_table_row}
+
+
+def produce_download_tables(mode="rst"):
     sizes = ["medium", "small"]
 
     runs = {
@@ -39,28 +67,24 @@ def produce_download_tables():
 
     tables = []
     for size in sizes:
-        table = (
-            f".. csv-table:: GPT-2 {size.capitalize()} Models\n"
-            '   :header: "Run", "Type", "Checkpoint", "Size", "Link"\n'
-            "   :widths: 7,7,7,5,7\n\n"
-        )
+        table = table_header_creators[mode](size)
         for run in sorted(runs[size]):
             for checkpoint in sorted(checkpoints, reverse=True):
                 # build and verify download link
                 download_link = f"https://storage.googleapis.com/mistral-models/gpt2-{size}/{run.lower()}-gpt2-{size}-{run_to_id[run]}/{run.lower()}-checkpoint-{checkpoint}.zip"
                 # assert verify_download_link(download_link), f"link failed: {download_link}"
                 # add row
-                table += (
-                    f'   "{run}", "GPT-2 {size.capitalize()}", "{checkpoint}", {download_sizes[size]}, `download'
-                    f" <{download_link}>`_\n"
-                )
+                table += row_creators[mode](run, size, checkpoint, download_sizes[size], download_link)
         tables.append(table)
 
     return tables
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", choices=["github", "rst"], help="type of table to build", default="rst")
+    args = parser.parse_args()
     print("")
-    for table in produce_download_tables():
+    for table in produce_download_tables(mode=args.mode):
         print(table)
     print("")
