1684c5f832fe71b311f07d27e73a3ce8fde4f8b8
David Hall <dlwh@stanford.edu>
2022-03-07 12:35:07 -0800

    wip

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -14,7 +14,7 @@ TRAIN_ARGS_SEED_7 = {
     "nnodes": "1",
     "nproc_per_node": "1",
     "config": "conf/train.yaml",
-    "training_arguments.fp16": "true",
+    "training_arguments.fp16": "false",
     "training_arguments.per_device_train_batch_size": "1",
     "artifacts.cache_dir": CACHE_DIR,
     "seed": "7",
@@ -37,7 +37,6 @@ def is_randomized(key):
     weights and biases, and bias terms in general.
     """
     # regexes for components that are not randomized
-    print(key)
     if key.endswith("bias") or "ln" in key:
         return False
     else:
@@ -57,110 +56,14 @@ def test_data_order() -> None:
     seed_7_dataloader = trainer_seed_7.get_train_dataloader()
     seed_10_dataloader = trainer_seed_10.get_train_dataloader()
     seed_7_indices, seed_10_indices = list(iter(seed_7_dataloader.sampler)), list(iter(seed_10_dataloader.sampler))
-    expected_indices = [
-        7485,
-        6448,
-        8289,
-        7940,
-        6492,
-        4866,
-        1722,
-        1303,
-        3568,
-        7713,
-        4597,
-        3294,
-        7178,
-        2517,
-        8770,
-        8208,
-        90,
-        4594,
-        4487,
-        5002,
-        2784,
-        4846,
-        6457,
-        4210,
-        1510,
-        2230,
-        8074,
-        1846,
-        753,
-        3613,
-        3354,
-        8174,
-        6577,
-        6422,
-        2463,
-        670,
-        8784,
-        8659,
-        2515,
-        647,
-        6654,
-        5255,
-        8623,
-        7172,
-        679,
-        4060,
-        4177,
-        2159,
-        7638,
-        3163,
-        468,
-        2689,
-        5817,
-        8100,
-        5736,
-        8081,
-        3993,
-        7968,
-        3549,
-        7995,
-        596,
-        370,
-        6044,
-        1640,
-        1693,
-        7685,
-        3544,
-        5806,
-        1887,
-        692,
-        5526,
-        4601,
-        3042,
-        8700,
-        222,
-        1601,
-        4908,
-        5576,
-        4823,
-        7853,
-        6892,
-        5932,
-        7890,
-        2599,
-        6431,
-        2136,
-        8601,
-        964,
-        2214,
-        3320,
-        1593,
-        5543,
-        5599,
-        1694,
-        3991,
-        3595,
-        4128,
-        5573,
-        4720,
-        4600,
-    ]
-    actual_indices = [seed_10_indices.index(seed_7_indices[i]) for i in range(0, 100)]
-    assert expected_indices == actual_indices, actual_indices
+
+    trainer_seed_7_copy = run_train_process(cl_args_dict=TRAIN_ARGS_SEED_7, runs_dir=RUNS_DIR,
+                                            run_id="trainer_seed_7_copy")
+    seed_7_copy_dataloader = trainer_seed_7_copy.get_train_dataloader()
+    seed_7_copy_indices = list(iter(seed_7_copy_dataloader.sampler))
+
+    assert seed_7_copy_indices == seed_7_indices, "data order is different"
+    assert seed_10_indices != seed_7_indices, "data order should be different for different seeds"
 
 
 if __name__ == "__main__":
