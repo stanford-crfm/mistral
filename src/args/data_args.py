d3115dc184e3605c54089814a3f57ca9b857d56b
J38 <jebolton@stanford.edu>
2022-03-09 04:53:12 -0800

    update how data_files handled

--- src/args/data_args.py
+++ src/args/data_args.py
@@ -20,10 +20,9 @@ def get_data_arguments(quinfig_args: Munch) -> Dict[Any, Any]:
     if quinfig_args.datasets:
         data_args["dataset_list"] = [dict(ds) for ds in quinfig_args.datasets]
         # set up data_files
-        for ds in data_args["dataset_list"]:
-            data_files = {"train": ds.pop("train", None), "validation": ds.pop("validation", None)}
-            if data_files["train"] or data_files["validation"]:
-                data_args["data_files"] = data_files
+        for ds in data_args:
+            if "data_files" in ds:
+                ds["data_files"] = dict(ds["data_files"])
     else:
         data_args["dataset_list"] = [{"path": quinfig_args.id, "name": quinfig_args.name}]
     # get ratios of datasets
