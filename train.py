0c04180cc567a69e5c302cf22b14c2f79ca6997c
David Hall <dlwh@stanford.edu>
2022-05-02 10:34:22 -0700

    broaden the values we accept to disable wandb

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ def train() -> OnlineBenchmarkTrainer:
     callbacks = [
         CustomCheckpointCallback(frequencies=frequencies),
     ]
-    if os.getenv("WANDB_DISABLED", "false").lower() != "true":
+    if os.getenv("WANDB_DISABLED", "false").lower() not in ["true", "1", "yes"]:
         callbacks.append(
             CustomWandbCallback(
                 quinfig.wandb,
