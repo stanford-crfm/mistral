7ba723ef9f6979bbd2017c591d40de0df3173d53
J38 <jebolton@stanford.edu>
2022-05-12 02:30:58 -0700

    add y to create cmd

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,7 @@ if  [ "$CONDA_DEFAULT_ENV" != "base" ]; then
     echo "Error: run setup from (base) environment!"
     exit
 fi
-conda create -n mistral --file conda-requirements.txt -c pytorch
+conda create -y -n mistral --file conda-requirements.txt -c pytorch
 . $CONDA_PREFIX/etc/profile.d/conda.sh
 conda activate mistral
 pip install -r pip-requirements.txt
