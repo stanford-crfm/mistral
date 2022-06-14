1cf363a515533acc5ccfdf6448fd15738e4f6dd5
J38 <jebolton@stanford.edu>
2022-06-14 03:07:05 -0700

    fix cache bug

--- src/corpora/auto_mlm.py
+++ src/corpora/auto_mlm.py
@@ -38,7 +38,7 @@ def get_auto_mlm_dataset(
     stride = seq_len if stride < 0 else stride
     assert stride <= seq_len, f"Data grouping stride ({stride}) is smaller than sequence length: we are losing data."
     dataset = datasets.load_dataset(
-        dataset_id, name=dataset_name, cache_dir=str(paths["dataset"]), keep_in_memory=True
+        dataset_id, name=dataset_name, cache_dir=str(paths["dataset"])
     )
 
     if "validation" not in dataset:
