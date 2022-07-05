7ba12ef130f5a155cddfd8081bfda3a84e79f04c
David Hall <dlwh@stanford.edu>
2022-07-05 15:42:57 -0700

    make detokenization function hashable to please HF datasets

--- src/corpora/detokenization.py
+++ src/corpora/detokenization.py
@@ -12,49 +12,50 @@ from typing import Dict
 overwatch = logging.getLogger("mistral.corpora.detokenization")
 
 
-def wikitext_detokenize(example: Dict[str, str]) -> Dict[str, str]:
-    """
-    Wikitext is whitespace tokenized and we remove these whitespaces.
+class WikitextDetokenize():
+    def __call__(self, example: Dict[str, str]) -> Dict[str, str]:
+        """
+        Wikitext is whitespace tokenized and we remove these whitespaces.
 
-    Taken from https://github.com/NVIDIA/Megatron-LM/blob/main/tasks/zeroshot_gpt2/detokenizer.py
-    """
-    # Contractions
-    text = example["text"]
-    text = text.replace("s '", "s'")
-    text = re.sub(r"/' [0-9]/", r"/'[0-9]/", text)
+        Taken from https://github.com/NVIDIA/Megatron-LM/blob/main/tasks/zeroshot_gpt2/detokenizer.py
+        """
+        # Contractions
+        text = example["text"]
+        text = text.replace("s '", "s'")
+        text = re.sub(r"/' [0-9]/", r"/'[0-9]/", text)
 
-    # Number Separators
-    text = text.replace(" @-@ ", "-")
-    text = text.replace(" @,@ ", ",")
-    text = text.replace(" @.@ ", ".")
+        # Number Separators
+        text = text.replace(" @-@ ", "-")
+        text = text.replace(" @,@ ", ",")
+        text = text.replace(" @.@ ", ".")
 
-    # Punctuation
-    text = text.replace(" : ", ": ")
-    text = text.replace(" ; ", "; ")
-    text = text.replace(" . ", ". ")
-    text = text.replace(" ! ", "! ")
-    text = text.replace(" ? ", "? ")
-    text = text.replace(" , ", ", ")
+        # Punctuation
+        text = text.replace(" : ", ": ")
+        text = text.replace(" ; ", "; ")
+        text = text.replace(" . ", ". ")
+        text = text.replace(" ! ", "! ")
+        text = text.replace(" ? ", "? ")
+        text = text.replace(" , ", ", ")
 
-    # Double Brackets
-    text = re.sub(r"\(\s*([^\)]*?)\s*\)", r"(\1)", text)
-    text = re.sub(r"\[\s*([^\]]*?)\s*\]", r"[\1]", text)
-    text = re.sub(r"{\s*([^}]*?)\s*}", r"{\1}", text)
-    text = re.sub(r"\"\s*([^\"]*?)\s*\"", r'"\1"', text)
-    text = re.sub(r"'\s*([^']*?)\s*'", r"'\1'", text)
+        # Double Brackets
+        text = re.sub(r"\(\s*([^\)]*?)\s*\)", r"(\1)", text)
+        text = re.sub(r"\[\s*([^\]]*?)\s*\]", r"[\1]", text)
+        text = re.sub(r"{\s*([^}]*?)\s*}", r"{\1}", text)
+        text = re.sub(r"\"\s*([^\"]*?)\s*\"", r'"\1"', text)
+        text = re.sub(r"'\s*([^']*?)\s*'", r"'\1'", text)
 
-    # Miscellaneous
-    text = text.replace("= = = =", "====")
-    text = text.replace("= = =", "===")
-    text = text.replace("= =", "==")
-    text = text.replace(" " + chr(176) + " ", chr(176))
-    text = text.replace(" \n", "\n")
-    text = text.replace("\n ", "\n")
-    text = text.replace(" N ", " 1 ")
-    text = text.replace(" 's", "'s")
+        # Miscellaneous
+        text = text.replace("= = = =", "====")
+        text = text.replace("= = =", "===")
+        text = text.replace("= =", "==")
+        text = text.replace(" " + chr(176) + " ", chr(176))
+        text = text.replace(" \n", "\n")
+        text = text.replace("\n ", "\n")
+        text = text.replace(" N ", " 1 ")
+        text = text.replace(" 's", "'s")
 
-    return {"text": text}
+        return {"text": text}
 
 
 # Set Registry for Various Datasets
-DATASET_TOKENIZATION_REGISTRY = {"wikitext": wikitext_detokenize}
+DATASET_TOKENIZATION_REGISTRY = {"wikitext": WikitextDetokenize()}
