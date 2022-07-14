fd4b6ceb58a5b62e09642ca3b7253eb641b147f8
David Hall <dlwh@stanford.edu>
2022-07-14 15:46:18 -0700

    refactor setup scripts to be merged, don't hardcode runner path.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+ENV_NAME="mistral"
+# if we have an arg to the script, use it as the env name
+if [ $# -eq 1 ]; then
+    ENV_NAME=$1
+fi
 
 if  [ "$CONDA_DEFAULT_ENV" != "base" ]; then
     echo "Error: run setup from base environment!"
     exit
 fi
-echo "Creating mistral conda environment!"
-conda create -y -n mistral --file conda-requirements.txt -c pytorch
+echo "Creating mistral conda environment '${ENV_NAME}'!"
+conda create -y -n "${ENV_NAME}" --file conda-requirements.txt -c pytorch
 . $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate mistral
-if [ "$CONDA_DEFAULT_ENV" = "mistral" ]; then
+conda activate "${ENV_NAME}"
+if [ "$CONDA_DEFAULT_ENV" = "${ENV_NAME}" ]; then
     echo "Installing python dependencies with pip!"
     pip install -r pip-requirements.txt
 fi
-echo "Successfully created mistral environment!"
+echo "Successfully created mistral environment '${ENV_NAME}'!"
+
