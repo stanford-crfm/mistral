7f1a34624230d65d08daceb3e9a765eeda0e64b2
J38 <jebolton@stanford.edu>
2022-07-18 23:42:40 -0700

    add mistral models

--- src/util/registry.py
+++ src/util/registry.py
@@ -6,7 +6,14 @@ more model configurations, different types of architectures, etc.
 """
 
 # Model Names
-REGISTRY = {"gpt2-small": "gpt2", "gpt2-medium": "gpt2-medium", "gpt2-large": "gpt2-large", "gpt2-xl": "gpt2-xl"}
+REGISTRY = {
+    "gpt2-small": "gpt2",
+    "gpt2-medium": "gpt2-medium",
+    "gpt2-large": "gpt2-large",
+    "gpt2-xl": "gpt2-xl",
+    "mistral-small": "gpt2",
+    "mistral-medium": "gpt2-medium",
+}
 
 # Absolute Paths
 PATH_REGISTRY = {"gpt2-small": "gpt2", "gpt2-medium": "gpt2", "gpt2-large": "gpt2", "gpt2-xl": "gpt2"}
