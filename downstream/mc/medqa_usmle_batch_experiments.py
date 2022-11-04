2883857f9b3c6434e3ecd927eba1fd9b9cfb35c7
J38 <jebolton@stanford.edu>
2022-11-03 21:48:43 -0700

    fix dev/test file issue

--- downstream/mc/medqa_usmle_batch_experiments.py
+++ downstream/mc/medqa_usmle_batch_experiments.py
@@ -20,19 +20,21 @@ for experiment in experiments:
             tokenizer_setting = ""
         else:
             tokenizer_setting = f" --tokenizer_name {experiment['tokenizer']} "
+    seed = experiment["seed"] if "seed" in experiment else 7
     name = experiment["name"]
     subprocess.call(f"mkdir -p /u/scr/nlp/data/mercury/pubmed/xl-model-eval/medqa/runs/{name}", shell=True)
     exp_cmd = (
         "python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0 run_multiple_choice.py"
         f" {tokenizer_setting} --model_name_or_path"
         f" {checkpoint} --train_file $datadir/train.json"
-        " --validation_file $datadir/test.json --test_file $datadir/test.json --do_train --do_eval --do_predict"
+        " --validation_file $datadir/dev.json --test_file $datadir/test.json --do_train --do_eval --do_predict"
         f" --per_device_train_batch_size 2 --per_device_eval_batch_size 1 --gradient_accumulation_steps {grad_accum}"
         f" --learning_rate {lr} --warmup_ratio 0.5 --num_train_epochs {num_train_epochs} --max_seq_length 512 --fp16"
         " --logging_first_step --logging_steps 20 --save_strategy no --evaluation_strategy steps "
         " --eval_steps 100 --output_dir"
         f" /u/scr/nlp/data/mercury/pubmed/xl-model-eval/medqa/runs/{name} --overwrite_output_dir"
         f" --sharded_ddp zero_dp_2 --run_name {name}_task=medqa_usmle_hf"
+        f" --seed {seed} --data_seed {seed}"
     )
     print(exp_cmd)
     try:
