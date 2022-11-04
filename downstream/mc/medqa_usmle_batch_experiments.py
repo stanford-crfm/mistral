6b9beb56114a6406686ada19a8b1b5cb5bf614d7
J38 <jebolton@stanford.edu>
2022-11-03 20:41:06 -0700

    latest settings

--- downstream/mc/medqa_usmle_batch_experiments.py
+++ downstream/mc/medqa_usmle_batch_experiments.py
@@ -1,47 +1,35 @@
+import json
 import subprocess
+import sys
 
 env_setup_cmd = "task=medqa_usmle_hf ; datadir=data/$task ; export WANDB_PROJECT='biomedical-nlp-eval'"
 
-experiments = [
-    #{
-        #"lr": "3e-5",
-        #"checkpoint": "/u/scr/nlp/data/mercury/pubmed/checkpoints/xl/gpt2-xl",
-        #"num_train_epochs": 30,
-        #"name": "gpt-xl-english-lr=3e-5",
-    #},
-    #{
-        #"lr": "3e-5",
-        #"checkpoint": "/u/scr/nlp/data/mercury/pubmed/checkpoints/xl/pubmed_gpt_xl_no_clean_100k",
-        #"num_train_epochs": 60,
-        #"name": "gpt-xl-pubmed-lr=3e-5-num_epochs=60",
-    #},
-    {
-        "lr": "3e-5",
-        "checkpoint": "/u/scr/nlp/data/mercury/pubmed/checkpoints/xl/pubmed_gpt_xl_180k",
-        "num_train_epochs": 30,
-        "name": "pubmed_gpt_xl_180k_lr=3e-5_epochs=30",
-    },
-    
-]
+experiments = [json.loads(line) for line in open(sys.argv[1]).read().split("\n")[:-1]]
 
-#checkpoints = ["composer-pubmed-285k", "composer-pubmed-250k", "composer-pubmed-225k", "composer-pubmed-208k", "composer-pubmed-openweb-400k", "composer-pubmed-openweb-350k", "composer-pubmed-openweb-302k", "composer-pubmed-openweb-250k", "composer-pubmed-openweb-200k", "composer-pubmed-openweb-150k", "composer-pubmed-openweb-100k"]
+print(experiments[-1])
 
-#experiments = [{"lr": "3e-5", "checkpoint": f"/u/scr/nlp/data/mercury/pubmed/checkpoints/small/{checkpoint}", "num_train_epochs": 30, "name": f"gpt-small-{checkpoint}"} for checkpoint in checkpoints] 
 
 for experiment in experiments:
     lr = experiment["lr"]
     checkpoint = experiment["checkpoint"]
+    grad_accum = experiment["grad_accum"]
     num_train_epochs = experiment["num_train_epochs"]
+    tokenizer_setting = " --tokenizer_name gpt2 "
+    if "tokenizer" in experiment:
+        if experiment["tokenizer"] == "":
+            tokenizer_setting = ""
+        else:
+            tokenizer_setting = f" --tokenizer_name {experiment['tokenizer']} "
     name = experiment["name"]
     subprocess.call(f"mkdir -p /u/scr/nlp/data/mercury/pubmed/xl-model-eval/medqa/runs/{name}", shell=True)
     exp_cmd = (
         "python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0 run_multiple_choice.py"
-        " --tokenizer_name gpt2 --model_name_or_path"
+        f" {tokenizer_setting} --model_name_or_path"
         f" {checkpoint} --train_file $datadir/train.json"
         " --validation_file $datadir/test.json --test_file $datadir/test.json --do_train --do_eval --do_predict"
-        " --per_device_train_batch_size 1 --per_device_eval_batch_size 1 --gradient_accumulation_steps 4"
+        f" --per_device_train_batch_size 2 --per_device_eval_batch_size 1 --gradient_accumulation_steps {grad_accum}"
         f" --learning_rate {lr} --warmup_ratio 0.5 --num_train_epochs {num_train_epochs} --max_seq_length 512 --fp16"
-        " --logging_first_step --logging_steps 20 --save_strategy steps --evaluation_strategy steps --save_steps 300"
+        " --logging_first_step --logging_steps 20 --save_strategy no --evaluation_strategy steps "
         " --eval_steps 100 --output_dir"
         f" /u/scr/nlp/data/mercury/pubmed/xl-model-eval/medqa/runs/{name} --overwrite_output_dir"
         f" --sharded_ddp zero_dp_2 --run_name {name}_task=medqa_usmle_hf"
