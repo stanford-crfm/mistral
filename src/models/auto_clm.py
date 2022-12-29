b4e754482afce9695174fe01e83dd3802843a07f
J38 <jebolton@stanford.edu>
2022-12-28 20:49:39 -0800

    february 2022 code wit bug fixes and flash

--- src/models/auto_clm.py
+++ src/models/auto_clm.py
@@ -13,7 +13,6 @@ import torch
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
 from transformers.models.gpt2 import GPT2Config, GPT2LMHeadModel
 
-from ..corpora.tokenization_utils import PassthroughTokenizer
 from ..util import REGISTRY
 
 
@@ -25,8 +24,8 @@ def get_auto_clm_tokenizer(
     model_id: str,
     paths: Dict[str, Path],
     model_configs: dict = None,
+    gradient_checkpointing: bool = True,
     use_pretrained_tokenizer: bool = True,
-    use_passthrough_tokenizer: bool = False,
     reorder_and_upcast_attn: bool = True,
     scale_attn_by_inverse_layer_idx: bool = True,
     initial_weights: str = None,
@@ -37,35 +36,34 @@ def get_auto_clm_tokenizer(
     if "gpt2" in model_id and model_configs:
         overwatch.info(f"Building Hugging Face GPT2Config from provided configs: {model_configs} ...")
         config = GPT2Config.from_dict(model_configs)
+        config.reorder_and_upcast_attn = reorder_and_upcast_attn
+        config.scale_attn_by_inverse_layer_idx = scale_attn_by_inverse_layer_idx
     else:
         overwatch.info(f"Fetching Hugging Face AutoConfig for Model: `{REGISTRY[model_id]}`...")
         config = AutoConfig.from_pretrained(REGISTRY[model_id], cache_dir=paths["configs"])
 
-    # mistral config is just gpt2 with the following additional stability fixes
-    if "mistral" in model_id or "gpt2" in model_id:
-        config.reorder_and_upcast_attn = reorder_and_upcast_attn
-        config.scale_attn_by_inverse_layer_idx = scale_attn_by_inverse_layer_idx
-
     # IMPORTANT :: Set `use_cache` to False -- we don't need it ever and it conflicts with gradient checkpointing!
     config.use_cache = False
 
+    # Use faster gelu
+    config.activation_function = "gelu"
+
     # Create Tokenizer
     overwatch.info(f"Fetching Hugging Face [Fast] AutoTokenizer for Model: `{REGISTRY[model_id]}`...")
-    assert not (
-        use_pretrained_tokenizer and use_passthrough_tokenizer
-    ), "Pretrained and Passthrough tokenization are mutually exclusive"
     if use_pretrained_tokenizer:
         tokenizer = AutoTokenizer.from_pretrained(REGISTRY[model_id], config=config, cache_dir=paths["tokenizer"])
-    elif use_passthrough_tokenizer:
-        overwatch.info("Using a Pretokenized Dataset")
-        tokenizer = PassthroughTokenizer(config.vocab_size)
     else:
         overwatch.error("Tokenizer Training/Initialization (from Scratch) not yet implemented!")
         raise NotImplementedError()
 
+    # Partial Gradient Checkpointing (currently only supported for GPT-2 models)
     if "gpt2" in model_id:
         overwatch.info(f"Initializing Custom GPT-2 Model from Configuration: `{REGISTRY[model_id]}`...")
         model = GPT2LMHeadModel(config)
+        if gradient_checkpointing:
+            model.gradient_checkpointing_enable()
+
+    # No Adaptive Gradient Checkpointing
     else:
         # Initialize Model
         overwatch.info(f"Initializing Tabula Rasa Model from Configuration: `{REGISTRY[model_id]}`...")
@@ -78,5 +76,7 @@ def get_auto_clm_tokenizer(
     if initial_weights is not None:
         overwatch.info(f"Initializing Weights from File: `{initial_weights}`...")
         model.load_state_dict(torch.load(initial_weights, map_location=torch.device("cpu")))
-
+    
+    overwatch.info("Model config: ")
+    overwatch.info(model.config)
     return model, tokenizer
